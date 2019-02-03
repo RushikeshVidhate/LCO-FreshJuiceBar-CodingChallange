1e7c63fcd60dcb6a606812bccbcdee2d33b42d3f
Rushikesh V <www.rushikeshvidhate02@gmail.com>
2019-02-03 19:52:34 +0530

    Master - Done Basice Plus

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+// Navbar JS
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
